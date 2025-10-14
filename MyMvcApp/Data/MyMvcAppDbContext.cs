46fbc1ed0bc34453ff26553e49110d557a200bf0
Bug_In_TheMatrix <rsaw@ciklum.com>
2025-10-14 10:45:25 +0530

    first commit

---
+++ MyMvcApp/Data/MyMvcAppDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using MyMvcApp.Models.Domain;
+
+namespace MyMvcApp.Data
+{
+    public class MyMvcAppDbContext : DbContext
+    {
+        public MyMvcAppDbContext(DbContextOptions<MyMvcAppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        
+    }
+}
+
+
---
+++ MyMvcApp/Models/Domain/Tag.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyMvcApp.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public ICollection<BlogPost> BlogPosts { get; set; }
+
+    }
+}
+
+
